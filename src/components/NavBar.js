7c1f227f58de8b938625a1f464529275386d5eb5
nikolay2 <kolia.aleko@gmail.com>
2023-02-20 19:15:36 +0200

    finish first page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
 import styles from './NavBar.module.css';
 const NavBar = () => {
+    const logoIcon = '</>';
+    const logo = `WEB ${logoIcon} DEVELOPMENT`;
     return (
         <>
-            <div className={styles['nav-bar']}></div>;
+            <div className={styles['nav-bar']}>
+                <h1>
+                    WEB <span>{logoIcon}</span> DEVELOPMENT
+                </h1>
+                <div className={styles['header-links']}>
+                    <a href='#stack'>Stack of Technologies</a>
+                    <a href='#works'>Works</a>
+                    <a href='#contacts'>Contacts</a>
+                </div>
+            </div>
+            ;
         </>
     );
 };
