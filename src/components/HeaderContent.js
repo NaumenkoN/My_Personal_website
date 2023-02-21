2343bdafbf41164d4d7e16500e27175b2d21fc26
nikolay2 <kolia.aleko@gmail.com>
2023-02-21 12:38:55 +0200

    complite firs page

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -2,13 +2,20 @@ import styles from './HeaderContent.module.css';
 
 const HeaderContext = () => {
     return (
-        <div className={styles['header-context']}>
-            <h1>Hey!</h1>
-            <p>
-                lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
-                ipsumlorem ipsumlorem ipsumlorem ipsum{' '}
-            </p>
-        </div>
+        <>
+            <h1 className={styles.greetings}>
+                <span>I`M</span> NIKOLAI
+                <br />
+                WEB <span>DEVELOPER</span>
+            </h1>
+            <p className={styles.explanation}>Helping you and your busines to grow up</p>
+            <div className={styles['header-context']}>
+                <p>
+                    .header-context p import styles from './HeaderContent ,module.css'; [dataState, setDataState] = useState;(false);
+                    users.map((user)=> user.id === key && setDataState(true))
+                </p>
+            </div>
+        </>
     );
 };
 
