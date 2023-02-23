a058067dd0f490d245b12586580351231f1a5285
nikolay2 <kolia.aleko@gmail.com>
2023-02-23 10:31:46 +0200

    gg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
-
-import Header from './components/Header';
+import Main from './components/main/Main';
+import { useState } from 'react';
+import Stack from './components/StackPage/Stack';
+import ProjectPage from './components/projects/ProjectPage';
 
 function App() {
+    const [mainPageState, setMainPageState] = useState(true);
+    const [stackPageState, setStackPageState] = useState(false);
+    const [projectsPageState, setProjectsPageState] = useState(false);
+    const [underline, setUnderline] = useState(1);
+
+    const showStackPage = () => {
+        setStackPageState(true);
+        setMainPageState(false);
+        setProjectsPageState(false);
+        setUnderline(2);
+    };
+
+    const showMainPage = () => {
+        setMainPageState(true);
+        setStackPageState(false);
+        setProjectsPageState(false);
+        setUnderline(1);
+    };
+
+    const showProjectsPage = () => {
+        setMainPageState(false);
+        setStackPageState(false);
+        setProjectsPageState(true);
+        setUnderline(3);
+    };
+
     return (
         <React.Fragment>
-            <Header />
+            {mainPageState && <Main showStackPage={showStackPage} showProjectsPage={showProjectsPage} underline={underline} />}
+
+            {stackPageState && <Stack showMainPage={showMainPage} showProjectsPage={showProjectsPage} underline={underline} />}
+
+            {projectsPageState && <ProjectPage showStackPage={showStackPage} showMainPage={showMainPage} underline={underline} />}
         </React.Fragment>
     );
 }
